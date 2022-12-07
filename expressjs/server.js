831815fb184b84ca3e675a6838a98300c7967752
Vishkash <vishkash.vishnu@gmail.com>
2022-12-07 20:23:09 +0530

    Add Favicon & 404

--- expressjs/server.js
+++ expressjs/server.js
@@ -4,16 +4,28 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  "/favicon.ico",
+  express.static(path.join(__dirname, "public", "images", "favicon.ico"))
+);
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
-app.use("/", (req, res) => {
+app.get("/", (req, res) => {
   res.render("pages/HomePage.ejs");
+});
+
+app.use("*", (req, res) => {
+  res.status(404).send({
+    response: "no such page",
+    status: 404,
+    "more@": "https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/404",
+  });
 });
 
 app.listen(PORT, () => {
