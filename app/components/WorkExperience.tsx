465300ebaf232351de6c6414412e2fe6fa2b8753
vishkashpvp <vishkash.k@gmail.com>
2024-10-29 23:53:03 +0530

    refactor(icons): move SVGs to separate files for better organization

--- app/components/WorkExperience.tsx
+++ app/components/WorkExperience.tsx
@@ -1,4 +1,7 @@
 import Link from "next/link";
+import CalendarIcon from "@icons/CalendarIcon";
+import ClockIcon from "@icons/ClockIcon";
+import BriefcaseIcon from "@icons/BriefcaseIcon";
 
 export default function WorkExperience() {
   return (
@@ -8,19 +11,7 @@ export default function WorkExperience() {
           <h2 className="mb-5 text-3xl font-bold">work experience</h2>
           <div className="mb-6 text-sm">
             <div className="flex items-center p-3 bg-gray-200 rounded dark:bg-white/10">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-4 h-4 mr-2"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor">
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
+              <ClockIcon />
               <span className="font-bold">overall experience: 2 years 4 months</span>
             </div>
           </div>
@@ -28,76 +19,28 @@ export default function WorkExperience() {
             <div>
               <h3 className="mb-2 text-xl font-semibold">software development engineer II</h3>
               <div className="flex items-center text-sm">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="w-4 h-4 mr-2"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor">
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
-                  />
-                </svg>
+                <CalendarIcon />
                 <span>september 2024 - present</span>
               </div>
             </div>
             <div>
               <h3 className="mb-2 text-xl font-semibold">software developer</h3>
               <div className="flex items-center text-sm">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="w-4 h-4 mr-2"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor">
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
-                  />
-                </svg>
+                <CalendarIcon />
                 <span>july 2023 - july 2024</span>
               </div>
             </div>
             <div>
               <h3 className="mb-2 text-xl font-semibold">get (graduate engineer trainee)</h3>
               <div className="flex items-center text-sm">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="w-4 h-4 mr-2"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor">
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
-                  />
-                </svg>
+                <CalendarIcon />
                 <span>june 2022 - june 2023</span>
               </div>
             </div>
           </div>
           <div className="mt-6 space-y-2 text-sm">
             <div className="flex items-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-4 h-4 mr-2"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor">
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
-                />
-              </svg>
+              <BriefcaseIcon />
               <span>
                 company:{" "}
                 <Link
@@ -110,19 +53,7 @@ export default function WorkExperience() {
               </span>
             </div>
             <div className="flex items-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-4 h-4 mr-2"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor">
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
+              <ClockIcon />
               <span>experience: 2 years 4 months</span>
             </div>
           </div>
